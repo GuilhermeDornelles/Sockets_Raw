e400b4418f3afbb7189dacd8f7cce10738e6fc12
Arthur Kunzler <arthur.gabriel.k@gmail.com>
2023-08-28 22:49:47 -0300

    add initial changes to pack creation done on lab

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from rawSocket import RawSocket
+
+if __name__ == "__main__":
+    socket = RawSocket(payload="Payload teste")
+    # socket.send_package()
--- src/rawSocket.py
+++ src/rawSocket.py
@@ -1,20 +1,42 @@
-from socket import socket, SOCK_RAW, AF_PACKET, htons
+# from socket import socket, SOCK_RAW, AF_PACKET, htons
+import socket
 import struct
 
 
 class RawSocket:
     def __init__(self, protocol=0x0800):
-        self.socket = socket(AF_PACKET, SOCK_RAW, htons(protocol))
+        self.socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.IPPROTO_IP)
         self.source_addresses = ""
         self.dest_addresses = ""
-        self.payload = ("["*30)+"PAYLOAD"+("]"*30)
+        self.payload = "[ "+"PAYLOAD"+" ]"
         self.set_interface(protocol)
-        pass
+        self.package = self._create_package()
+
+        
 
     def set_interface(self, protocol):
-        self.socket.bind("eth0", htons(protocol))
+        self.socket.bind("eth0", socket.IPPROTO_IP)
 
     def create_package(self):
+        version = 4
+        ihl = 5
+        tos = 0
+        length = 20 +len(self.payload)
+        id = 0
+        offset = 0
+        ttl = 255
+        checksum = 0
+        flags = 0
+
+        ver_ihl = (version << 4) + ihl
+        flags_offset = (flags << 13) + offset
+
         # 6 dest address, 6 source address and 2 for ethtype = IP
-        struct.pack("!6s6s2s", '\xaa\xaa\xaa\xaa\xaa\xaa',
-                    '\xbb\xbb\xbb\xbb\xbb\xbb', '\x08\x00')
+        pack = struct.pack("!6s6sH", b'\xa4\x1f\x72\xf5\x90\x52', b'\xa4\x1f\x72\xf5\x90\x98', 0x0800)
+        package = pack
+        sent = self.socket.send(package)
+        print(sent)
+
+    
+    def send_package(self):
+        self.socket.send(self.package)
