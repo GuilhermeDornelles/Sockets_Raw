d3698f22a202a7f873a358477baf667a166719ca
guilherme <guilherme.dornelles@edu.pucrs.br>
2023-08-31 15:40:36 -0300

    modifying main file

--- src/main.py
+++ src/main.py
@@ -2,26 +2,27 @@ from rawSocket import RawSocket
 from constants import IPV4_TYPE
 
 CONFIG = {
-    "MAC_ORIGEM": "a4:1f:72:f5:90:52",
+    # "MAC_ORIGEM": "a4:1f:72:f5:90:52",
+    "MAC_ORIGEM": "02:42:24:86:f7:fd",
     "MAC_DESTINO": "a4:1f:72:f5:90:98",
-    "IP_ORIGEM": "",
-    "IP_DESTINO": "",
+    "IP_ORIGEM": "192.168.0.1",
+    "IP_DESTINO": "192.168.0.1",
     "INTERFACE_REDE": "eth0"
-    # "INTERFACE_REDE": "Ethernet 2"
 }
 
 
 def main():
     socket = RawSocket(source_mac=CONFIG["MAC_ORIGEM"],     # Substituir pelos endereços MAC desejados
                        dest_mac=CONFIG["MAC_DESTINO"],
-                       net_interface=CONFIG["INTERFACE_REDE"],  # Substituir pelo nome da interface de rede da máquina
-                       # protocol=IPV4_TYPE
+                       source_ip=CONFIG["IP_ORIGEM"],
+                       dest_ip=CONFIG["IP_DESTINO"],
+                       # Substituir pelo nome da interface de rede da máquina
+                       net_interface=CONFIG["INTERFACE_REDE"],
+                       protocol=IPV4_TYPE  # Remover caso for usar apenas ethernet
                        )
-    
-    print(socket.eth_header)
-    # socket.send_package(data="mensagem de teste")
+
+    socket.send_package(data="mensagem de teste")
 
 
 if __name__ == "__main__":
     main()
-    
